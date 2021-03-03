9cae85bdb8c90d0d6afcdbe5c43c379a0feb6fa1
Omar Dario <melendrezomar2@gmail.com>
2021-03-03 07:54:53 -0300

    agregando formulario para agregar/actualizar/borrar mascotas del usuario

--- src/components/FormMascota.js
+++ src/components/FormMascota.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { store } from "../FirebaseConfig";
 
 const FormMascota = () => {
+  const [modoEdicion, setModoEdicion] = useState(null);
+  const [idMascota, setIdMascota] = useState("");
   const [nombre, setNombre] = useState("");
   const [raza, setRaza] = useState("");
   const [error, setError] = useState("");
@@ -36,7 +38,7 @@ const FormMascota = () => {
     console.log(mascotasUsuario);
     try {
       //Tomando la información del estado del formulario y agregando una mascota más por el usuario.
-      const data = await store.collection("mascotas").add(mascotasUsuario);
+      await store.collection("mascotas").add(mascotasUsuario);
       //solicitamos informacion de la coleccion
       const { docs } = await store.collection("mascotas").get();
       //guardamos los datos de la colección en un nuevo array
@@ -67,12 +69,59 @@ const FormMascota = () => {
     }
   };
 
+  const actualizarMascota = async (id) => {
+    try {
+      //solicitamos la data a la base de datos del elemento a modificar
+      const data = await store.collection("mascotas").doc(id).get();
+      const { nombre, raza } = data.data();
+      setNombre(nombre);
+      setRaza(raza);
+      setIdMascota(id);
+      setModoEdicion(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const agregarActualizacionMascota = async(e) => {
+    e.preventDefault();
+    if (!nombre.trim()) {
+      setError("El campo nombre está vacío");
+    }
+    if (!raza.trim()) {
+      setError("El campo raza está vacío");
+    }
+    const mascotaUpdate = {
+      nombre: nombre,
+      raza:raza
+    }
+    try {
+      //actualizamos el documento de mascota con la información que tenemos actualmente en el estado
+      await store.collection('mascotas').doc(idMascota).set(mascotaUpdate);
+      //actualizamos lista solicitando la coleccion a la base de datos
+      const { docs } = await store.collection("mascotas").get();
+      //guardamos los datos de la colección en un nuevo array
+      const nuevoArray = docs.map((item) => ({ id: item.id, ...item.data() }));
+      //seteamos en el estado los datos de la colección
+      setMascotas(nuevoArray);
+      alert("Mascota actualizada correctamente");
+      setModoEdicion(null);
+      setNombre("");
+      setRaza("");
+      setIdMascota("");
+    } catch (e) {
+      console.log(e)
+    }
+    
+  }
+  
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col">
           <h2>Formulario de Mascotas</h2>
-          <form onSubmit={agregarMascota} className="form-group">
+          <form onSubmit={modoEdicion ? agregarActualizacionMascota : agregarMascota} className="form-group">
             <input
               value={nombre}
               onChange={(e) => {
@@ -91,9 +140,15 @@ const FormMascota = () => {
               className="form-control mt-3"
               placeholder="Introduce raza"
             />
-            <button type="submit" className="btn btn-dark btn-block mt-3">
-              Registrar
-            </button>
+            {modoEdicion ? (
+              <button type="submit" className="btn btn-warning btn-block mt-3">
+                Actualizar
+              </button>
+            ) : (
+              <button type="submit" className="btn btn-dark btn-block mt-3">
+                Registrar
+              </button>
+            )}
           </form>
           {error ? (
             <div className="error">
@@ -109,9 +164,18 @@ const FormMascota = () => {
             {mascotas.map((mascota) => (
               <li key={mascota.id} className="list-group-item">
                 Nombre: {mascota.nombre} Raza: {mascota.raza}
-                <button 
-                onClick={(id) => borrarMascota(mascota.id)}
-                className="btn btn-danger float-right">Eliminar</button>
+                <button
+                  onClick={(id) => borrarMascota(mascota.id)}
+                  className="btn btn-danger float-right"
+                >
+                  Eliminar
+                </button>
+                <button
+                  onClick={(id) => actualizarMascota(mascota.id)}
+                  className="btn btn-info float-right mr-3"
+                >
+                  Actualizar
+                </button>
               </li>
             ))}
           </ul>
